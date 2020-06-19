eb847061f915402ef53ae026374c5b6bac5e7fc1
jnolan97 <johnmnolan97@gmail.com>
2020-06-19 17:55:12 -0500

    displays form now

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 import os
 from firebase_admin import credentials, firestore, initialize_app
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 from goodsend.forms import UserInfoForm
 from flask_login import login_required,login_user,current_user,logout_user
 from goodsend import app
-
+from goodsend.models import User, check_password_hash
 
 # Initialize Firestore DB
-cred = credentials.Certificate('./goodsend/key.json')
-default_app = initialize_app(cred)
-db = firestore.client()
-ben_ref = db.collection('beneficiary')
+# cred = credentials.Certificate('./goodsend/key.json')
+# default_app = initialize_app(cred)
+# db = firestore.client()
+# ben_ref = db.collection('beneficiary')
 # field_create = {
 #     "name": "test",
 #     "email": "testmail@test.com",
@@ -23,19 +23,28 @@ def create():
         Ensure you pass a custom ID as part of json body in post request
         e.g. json={'id': '1', 'title': 'Write a blog post'}
     """
-    try:
-        id = request.form['id']
-        # email = request.json['email']
-        # first_name = request.json['first_name']
-        # last_name = request.json['last_name']
-        ben_ref.document(id).set(request.form)
+    form = UserInfoForm()
+    if request.method == 'POST' and form.validate():
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+        email = form.email.data
+        password = form.password.data
+        user = User(first_name,last_name,email,password)
+        ben_ref.document().create(user)
+
+    # try:
+    #     id = request.form['id']
+    #     # email = request.json['email']
+    #     # first_name = request.json['first_name']
+    #     # last_name = request.json['last_name']
+    #     ben_ref.document(id).set(request.form)
         # ben_ref.document(email).set(request.json)
         # ben_ref.document(first_name).set(request.json)
         # ben_ref.document(last_name).set(request.json)
         # return jsonify({"success": True}), 200
-        return render_template("register.html"), jsonify({"success": True}), 200
-    except Exception as e:
-        return f"An Error Occured: {e}"
+    return render_template("register.html", form = form)
+    # except Exception as e:
+    #     return f"An Error Occured: {e}"
 # @app.route('/list', methods=['GET'])
 # def read():
 #     """
--- goodsend/models.py
+++ goodsend/models.py
@@ -1,4 +1,5 @@
 from goodsend import app, login
+from firebase_admin import credentials, firestore, initialize_app
 
 #Import for Werkzeug Security
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -7,3 +8,32 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 
 from flask_login import UserMixin
+@login.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(UserMixin):
+    # Initialize Firestore DB
+    cred = credentials.Certificate('./goodsend/key.json')
+    default_app = initialize_app(cred)
+    db = firestore.client()
+    ben_ref = db.collection('beneficiary')
+
+    # id = db.Column(db.Integer, primary_key = True)
+    # first_name = db.Column(db.String(150), nullable = False, unique = True)
+    # last_name = db.Column(db.String(150), nullable = False, unique = True)
+    # email = db.Column(db.String(150), nullable = False, unique = True)
+    # password = db.Column(db.String(256),nullable = False)
+
+    # def __init__(self,first_name,last_name,email,password):
+    #     self.first_name = first_name
+    #     self.last_name = last_name
+    #     self.email = email
+    #     self.password = self.set_password(password)
+
+    # def set_password(self,password):
+    #     self.pw_hash = generate_password_hash(password)
+    #     return self.pw_hash
+
+    # def __repr__(self):
+    #     return f'{self.first_name} has been created with {self.email}'
